d3141ac1af6745fdbf3f91fb550d72a94d09af59
tategakibunko <tategakibunko@localhost>
2010-04-13 09:47:37 +0900

    add paging

--- simple-reader.js
+++ simple-reader.js
@@ -1,13 +1,18 @@
 var SimpleReader = {
   start : function(){
+    var self = this;
     var text = document.getElementById("source-text").innerHTML.replace(/<br \/>/gi, "\n").replace(/<br>/gi, "\n");
     this.pageNo = 0;
+    this.writing = false;
     this.parser = new Nehan.StreamParser(new Nehan.Layout({
       width: 500,
       height: 400,
       fontSize: 16,
       fontFamily: "ＭＳ 明朝"
     }), new Nehan.TextStream(text, text.length, true));
+
+    document.getElementById("pager-next").onclick = function(){ self.next(); return false; };
+    document.getElementById("pager-prev").onclick = function(){ self.prev(); return false; };
 
     this.write(0);
   },
@@ -22,13 +27,13 @@ var SimpleReader = {
   next : function(){
     if(!this.writing && this.parser.hasNextPage()){
       this.pageNo++;
-      this.write(pageNo);
+      this.write(this.pageNo);
     }
   },
   prev : function(){
     if(!this.writing && this.pageNo > 0){
       this.pageNo--;
-      this.write(pageNo);
+      this.write(this.pageNo);
     }
   }
 }
