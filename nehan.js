49053d5f9d53ed2b1c5637fc6517844d163c444b
tategakibunko <tategakibunko@localhost>
2010-04-16 20:05:33 +0900

    some fix

--- nehan.js
+++ nehan.js
@@ -1509,7 +1509,7 @@ if(!Nehan.ParserHook){
   StreamParser.prototype.outputHalfWord = function(){
     if(this.halfBuff.lenght == 1){
       var ret = this.applyTagStack(this.halfBuff, true);
-    } else if(this.halfBuff.length <= 2 && !this.halfWordBreak && this.halfBuff.match(/\d+/)){
+    } else if(this.halfBuff.length <= 2 && !this.halfWordBreak && this.halfBuff.match(/\d+/) || this.halfBuff.match(/[!\?]+/)){
       var ret = this.applyTagStack(this.halfBuff, true);
     } else if (Env.isIE){
       var css = this.inlineCss({
