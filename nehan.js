d0f6915ef7a5de844f9cffd905d0f0973f5d7028
tategakibunko <tategakibunko@localhost>
2010-04-17 22:18:50 +0900

    add environment check

--- nehan.js
+++ nehan.js
@@ -70,13 +70,6 @@ if(!Nehan.ParserHook){
     }
   };
 
-  var Env = {
-    isIE : (document.all
-	    && window.ActiveXObject
-	    && navigator.userAgent.toLowerCase().indexOf("msie") > -1
-	    && navigator.userAgent.toLowerCase().indexOf("opera") == -1)
-  };
-
   var Option = {
     read : function(dst, defopt, opt){
       for(prop in defopt){
@@ -84,6 +77,148 @@ if(!Nehan.ParserHook){
       }
     }
   };
+
+  // http://www.quirksmode.org/js/detect.html
+  var BrowserDetect = {
+    init: function () {
+      this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+      this.version = this.searchVersion(navigator.userAgent)
+	|| this.searchVersion(navigator.appVersion)
+	|| "an unknown version";
+      this.OS = this.searchString(this.dataOS) || "an unknown OS";
+    },
+    searchString: function (data) {
+      for (var i=0;i<data.length;i++){
+	var dataString = data[i].string;
+	var dataProp = data[i].prop;
+	this.versionSearchString = data[i].versionSearch || data[i].identity;
+	if (dataString) {
+	  if (dataString.indexOf(data[i].subString) != -1)
+	    return data[i].identity;
+	}
+	else if (dataProp)
+	  return data[i].identity;
+      }
+    },
+    searchVersion: function (dataString) {
+      var index = dataString.indexOf(this.versionSearchString);
+      if (index == -1) return;
+	return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+    },
+    dataBrowser: [
+		  {
+		    string: navigator.userAgent,
+		    subString: "Chrome",
+		    identity: "Chrome"
+		  },
+		  { string: navigator.userAgent,
+		    subString: "OmniWeb",
+		    versionSearch: "OmniWeb/",
+		    identity: "OmniWeb"
+		  },
+		  {
+		    string: navigator.vendor,
+		    subString: "Apple",
+		    identity: "Safari",
+		    versionSearch: "Version"
+		  },
+		  {
+		    prop: window.opera,
+		    identity: "Opera"
+		  },
+		  {
+		    string: navigator.vendor,
+		    subString: "iCab",
+		    identity: "iCab"
+		  },
+		  {
+		    string: navigator.vendor,
+		    subString: "KDE",
+		    identity: "Konqueror"
+		  },
+		  {
+		    string: navigator.userAgent,
+		    subString: "Firefox",
+		    identity: "Firefox"
+		  },
+		  {
+		    string: navigator.vendor,
+		    subString: "Camino",
+		    identity: "Camino"
+		  },
+		  {// for newer Netscapes (6+)
+		    string: navigator.userAgent,
+		    subString: "Netscape",
+		    identity: "Netscape"
+		  },
+		  {
+		    string: navigator.userAgent,
+		    subString: "MSIE",
+		    identity: "Explorer",
+		    versionSearch: "MSIE"
+		  },
+		  {
+		    string: navigator.userAgent,
+		    subString: "Gecko",
+		    identity: "Mozilla",
+		    versionSearch: "rv"
+		  },
+		  { // for older Netscapes (4-)
+		    string: navigator.userAgent,
+		    subString: "Mozilla",
+		    identity: "Netscape",
+		    versionSearch: "Mozilla"
+		  }
+		  ],
+    dataOS : [
+	      {
+		string: navigator.platform,
+		subString: "Win",
+		identity: "Windows"
+	      },
+	      {
+		string: navigator.platform,
+		subString: "Mac",
+		identity: "Mac"
+	      },
+	      {
+		string: navigator.userAgent,
+		subString: "iPhone",
+		identity: "iPhone/iPod"
+	      },
+	      {
+		string: navigator.platform,
+		subString: "Linux",
+		identity: "Linux"
+	      }
+	      ]
+
+  };
+
+  BrowserDetect.init();
+
+  // public interface of environment variables.
+  var Env = {
+    init : function(){
+      var browser = BrowserDetect.browser.toLowerCase();
+      var version = BrowserDetect.version;
+
+      this.isIE = (browser == "explorer");
+
+      // check if browser can support transform method
+      if (browser == "chrome" || browser == "safari"){
+	this.canTransform = true;
+      } else if (browser == "firefox" && version >= 3.5){
+	this.canTransform = true;
+      } else if (this.isIE && version >= 6.0){
+	this.canTransform = (navigator.userAgent.toLowerCase().indexOf("windows nt 6") >= 0)? false : true;
+      } else {
+	this.canTransform = false;
+      }
+    }
+  };
+
+  Env.init();
 
   function Layout(option){
     Option.read(this, {
@@ -127,8 +262,7 @@ if(!Nehan.ParserHook){
     } else {
       this.lineCount = Math.floor(this.width / this.fontSize) - this.kinsokuCharCount;
     }
-    // if not Opera, maybe OK?
-    this.canTransform = (navigator.userAgent.toLowerCase().indexOf("opera") == -1);
+
     this.wrapCss = "";
     this.wrapCss += "text-align:left;";
     this.wrapCss += "padding:0;";
@@ -839,8 +973,7 @@ if(!Nehan.ParserHook){
       if(!this.layout.isV){
 	return 0.5;
       } else {
-	return (c == " ")? 1 : this.layout.canTransform? 0.5 : 1;
-	//return (this.layout.canTransform? 0.5 : 1);
+	return (c == " ")? 1 : Env.canTransform? 0.5 : 1;
       }
     }
   };
@@ -1560,7 +1693,7 @@ if(!Nehan.ParserHook){
 
     // add one char to current line.
     if(isV){
-      if(letterCount < 1 && this.layout.canTransform){
+      if(letterCount < 1 && Env.canTransform){
 	this.pushHalfChar(s1);
       } else {
 	this.lineBuff += this.applyTagStack(s1, true);
