ff2f4570a3c3e26c6e7a712fa2e16b5f2697115f
tategakibunko <tategakibunko@localhost>
2010-07-22 14:27:29 +0900

    add plugin example

---
+++ example/plugin.js
@@ -0,0 +1,15 @@
+if(Nehan){
+  Nehan.ParserHook.addTagHook("h2", function(pageNo, isV, tagStr, tagAttr, tagName){
+    this.parseFontStart(pageNo, isV, "<font scale='1.4' fontweight='bold'>", {scale:1.4, fontweight:"bold"}, "font");
+  });
+  Nehan.ParserHook.addTagHook("/h2", function(pageNo, isV, tagStr, tagAttr, tagName){
+    this.parseFontEnd(pageNo, isV, "</font>", tagAttr, "/font");
+  });
+  Nehan.ParserHook.addTagHook("alert", function(pageNo, isV, tagStr, tagAttr, tagName){
+    var href = "javascript:alert(\"" + tagAttr.msg + "\")";
+    this.parseLinkStart(pageNo, isV, "<a href='" + href + "'>", {href:href}, "a");
+  });
+  Nehan.ParserHook.addTagHook("/alert", function(pageNo, isV, tagStr, tagAttr, tagName){
+    this.parseLinkEnd(pageNo, isV, "</a>", tagAttr, "/a");
+  });
+};
