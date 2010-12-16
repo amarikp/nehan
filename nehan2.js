8eb86ab6b88a34c4ade4060e19d988bdcbad810c
tategakibunko <tategakibunko@localhost>
2010-12-17 08:11:27 +0900

    add some attribute for css positionning of img via ajax

--- nehan2.js
+++ nehan2.js
@@ -1,6 +1,6 @@
 /*
  source : nehan2.js
- version : 1.15
+ version : 1.16
  site : http://tategakibunko.mydns.jp/
  blog : http://tategakibunko.blog83.fc2.com/
 
@@ -28,7 +28,7 @@
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  OTHER DEALINGS IN THE SOFTWARE.
 */
-var Nehan;
+﻿var Nehan;
 
 if(!Nehan){
   Nehan = {};
