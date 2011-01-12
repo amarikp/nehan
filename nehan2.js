33fb9abe34c345d6713b942cd63dddb77bf5e990
tategakibunko <tategakibunko@localhost>
2011-01-12 15:33:36 +0900

    modify regexp for lexing of small characters

--- nehan2.js
+++ nehan2.js
@@ -533,8 +533,8 @@ if(!Nehan){
       return this.tag(p1, this.stream.readUntil(">"));
     } else if (c1=="&") {
       return this.character(p1, this.stream.readUntil(";"));
-    } else if(Env.canTransform && c1.match(/[0-9a-zA-Z!\.\?\/\_:]/)){
-      var str = this.stream.readMatch(/[0-9a-zA-Z!\.\?\/\_:]+/);
+    } else if(Env.canTransform && c1.match(/[0-9a-zA-Z!\.\?\/\_:#]/)){
+      var str = this.stream.readMatch(/[0-9a-zA-Z!\.\?\/\_:#]+/);
       if(str.length <= 2 && !isNaN(str) || str == "!?" || str == "!!" || str == "??"){
 	return this.tcy(p1, str);
       } else {
