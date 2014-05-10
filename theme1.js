fe68b5aaeafa89036296244160a974a156ae8ee0
tategakibunko <tategakibunko@localhost>
2014-05-11 00:39:25 +0900

    sample theme

---
+++ theme1.js
@@ -0,0 +1,32 @@
+Nehan.setStyles({
+  "body":{
+    "font-size":16,
+    "padding":"10px 16px",
+    "border-color":"wheat",
+    "border-style":"solid",
+    "border-width":"5px",
+    "border-radius":"5px",
+    "background-color":"wheat"
+  },
+  "blockquote":{
+    "color":"#444",
+    "background-color":"#dcdcdc",
+    "border-style":{
+      "start":"solid"
+    },
+    "border-color":{
+      "start":"#888"
+    },
+    "padding":{
+      "start":"5px"
+    },
+    "margin":{
+      "start":"10px",
+      "end":"10px"
+    },
+    "border-width":{
+      "start":"5px"
+    }
+  }
+});
+
