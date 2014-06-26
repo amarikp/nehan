9a8c204c8b9a6873f5f83869e6189d12851bda42
tategakibunko <tategakibunko@localhost>
2014-06-26 09:26:15 +0900

    update nehan4.min.js

--- nehan5.js
+++ nehan5.js
@@ -1318,14 +1318,14 @@ var List = {
   map : function(lst, fn){
     var ret = [];
     for(var i = 0, len = lst.length; i < len; i++){
-      ret[i] = fn(lst[i]);
+      ret.push(fn(lst[i]));
     }
     return ret;
   },
   mapi : function(lst, fn){
     var ret = [];
     for(var i = 0, len = lst.length; i < len; i++){
-      ret[i] = fn(i, lst[i]);
+      ret.push(fn(i, lst[i]));
     }
     return ret;
   },
@@ -2303,12 +2303,12 @@ var SelectorLexer = (function(){
     this.buff = this._normalize(src);
   }
 
-  var rex_name = /^[\w-_\*!\?]+/;
-  var rex_name_rex = /^\/[^\/]+\//;
-  var rex_id = /^#[\w-_]+/;
-  var rex_class = /^\.[\w-_]+/;
-  var rex_attr = /^\[[^\]]+\]/;
-  var rex_pseudo = /^:{1,2}[\w-_]+/;
+  var __rex_name = /^[\w-_\*!\?]+/;
+  var __rex_name_by_rex = /^\/[^\/]+\//;
+  var __rex_id = /^#[\w-_]+/;
+  var __rex_class = /^\.[\w-_]+/;
+  var __rex_attr = /^\[[^\]]+\]/;
+  var __rex_pseudo = /^:{1,2}[\w-_]+/;
 
   SelectorLexer.prototype = {
     getTokens : function(){
@@ -2353,12 +2353,18 @@ var SelectorLexer = (function(){
       return ret;
     },
     _getTypeSelector : function(){
+      var buff_len_before = this.buff.length;
       var name = this._getName();
       var name_rex = (name === null)? this._getNameRex() : null;
       var id = this._getId();
       var classes = this._getClasses();
       var attrs = this._getAttrs();
       var pseudo = this._getPseudo();
+
+      // if size of this.buff is not changed, there is no selector element.
+      if(this.buff.length === buff_len_before){
+	throw "invalid selector:[" + this.buff + "]";
+      }
       return new TypeSelector({
 	name:name,
 	nameRex:name_rex,
@@ -2369,20 +2375,20 @@ var SelectorLexer = (function(){
       });
     },
     _getName : function(){
-      return this._getByRex(rex_name);
+      return this._getByRex(__rex_name);
     },
     // type name defined by regexp
     // "/h[1-6]/.nehan-some-class span"
-    // => /h[16]/
+    // => /h[1-6]/
     _getNameRex : function(){
-      var name_rex = this._getByRex(rex_name_rex);
+      var name_rex = this._getByRex(__rex_name_by_rex);
       if(name_rex === null){
 	return null;
       }
       return new RegExp(name_rex.replace(/[\/]/g, ""));
     },
     _getId : function(){
-      var id = this._getByRex(rex_id);
+      var id = this._getByRex(__rex_id);
       return id? id.substring(1) : null;
     },
     _getClasses : function(){
@@ -2397,13 +2403,13 @@ var SelectorLexer = (function(){
       return classes;
     },
     _getClass : function(){
-      var klass = this._getByRex(rex_class);
+      var klass = this._getByRex(__rex_class);
       return klass? klass.substring(1) : null;
     },
     _getAttrs : function(){
       var attrs = [];
       while(true){
-	var attr = this._getByRex(rex_attr);
+	var attr = this._getByRex(__rex_attr);
 	if(attr === null){
 	  break;
 	}
@@ -2412,7 +2418,7 @@ var SelectorLexer = (function(){
       return attrs;
     },
     _getPseudo : function(){
-      var pseudo = this._getByRex(rex_pseudo);
+      var pseudo = this._getByRex(__rex_pseudo);
       return pseudo? new PseudoSelector(pseudo) : null;
     }
   };
@@ -7376,6 +7382,9 @@ var StyleContext = (function(){
     },
     getMarkupPos : function(){
       return this.markup.pos;
+    },
+    getMarkupData : function(name){
+      return this.markup.getData(name);
     },
     getContent : function(){
       var content = this.markup.getContent();
