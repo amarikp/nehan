6478cd4531bf87fbe1d3c503634156ad0f9c03e3
tategakibunko <tategakibunko@localhost>
2015-07-05 10:57:43 +0900

    update nehan4.min.js

--- nehan5.js
+++ nehan5.js
@@ -9359,13 +9359,13 @@ Nehan.HtmlLexer = (function (){
 
   HtmlLexer.prototype = {
     _normalize : function(src){
-      var src = src.replace(/(<\/.+?>)/gm, function(str, p1){
+      var src = src.replace(/(<\/[^>]+>)/gm, function(str, p1){
 	  return p1.toLowerCase();
       }); // convert close tag to lower case(for innerHTML of IE)
       src = __replace_single_close_tags(src);
       //src = src.replace(/“([^”]+)”/g, "〝$1〟") // convert double quote to double quotation mark
-      return src
-	.replace(/“(.*?)”/g, "”$1”")
+      src = src
+	.replace(/“([^”]+)”/g, "”$1”")
 	.replace(/｢/g, "「") // half size left corner bracket -> full size left corner bracket
 	.replace(/｣/g, "」") // half size right corner bracket -> full size right corner bracket
 	.replace(/､/g, "、") // half size ideographic comma -> full size ideographic comma
@@ -9373,6 +9373,8 @@ Nehan.HtmlLexer = (function (){
 	//.replace(/^[\s]+/, "") // shorten head space
 	//.replace(/[\s]+$/, "") // discard tail space
 	.replace(/\r/g, ""); // discard CR
+      //console.log("HtmlLexer::normalized to:", src);
+      return src;
     },
     /**
        @memberof Nehan.HtmlLexer
